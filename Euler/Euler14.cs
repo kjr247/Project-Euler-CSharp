bef887e18f3588f4478fd7e16857bfae43462bf2
Kyle Rebstock <kjr247@gmail.com>
2021-11-08 20:39:34 -0500

    first attampt to solve collatz longest chain

---
+++ Euler/Euler14.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Euler
+{
+    /// <summary>
+    /// The following iterative sequence is defined for the set of positive integers:
+    ///
+    /// n → n/2 (n is even)
+    /// n → 3n + 1 (n is odd)
+    ///
+    /// Using the rule above and starting with 13, we generate the following sequence:
+    ///
+    /// 13 → 40 → 20 → 10 → 5 → 16 → 8 → 4 → 2 → 1
+    /// It can be seen that this sequence (starting at 13 and finishing at 1) contains 10 terms. 
+    /// Although it has not been proved yet (Collatz Problem), it is thought that all starting numbers finish at 1.
+    ///
+    /// Which starting number, under one million, produces the longest chain?
+    ///
+    /// NOTE: Once the chain starts the terms are allowed to go above one million.
+    ///
+    /// </summary>
+    class Euler14
+    {
+
+        public static BigInteger Run(BigInteger n)
+        {
+            var original = n;
+            var longestChain = 0;
+            BigInteger currNum = 0;
+
+            while (n > 0)
+            {
+                var length = CalculateCallatz(n);
+                if (length > longestChain)
+                {
+                    longestChain = length;
+                    currNum = n;
+                }
+                n--;
+            }
+
+            return currNum;
+        }
+
+        // calculate - should always end at 1
+        public static int CalculateCallatz(BigInteger n)
+        {
+            var chainLength = 1;
+            while (n != 1)
+            {
+                chainLength++;
+                if (n % 2 == 0)
+                {
+                    n = n / 2;
+                }
+                else
+                {
+                    n = 3 * n + 1;
+                }
+            }
+            return chainLength;
+        }
+    }
+}
---
+++ Euler/Euler14Tests.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Euler;
+using System.Collections.Generic;
+using System.Numerics;
+
+namespace Euler
+{
+    [TestClass]
+    public class Euler14Tests
+    {
+
+        [TestMethod]
+        public void TestMethod0()
+        {
+            var expected = 10;
+            BigInteger results = Euler14.CalculateCallatz(13);
+            Assert.AreEqual(expected, results);
+        }
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var expected = 837799;
+            BigInteger results = Euler14.Run(1000000);
+            Assert.AreEqual(expected, results);
+        }
+    }
+}
